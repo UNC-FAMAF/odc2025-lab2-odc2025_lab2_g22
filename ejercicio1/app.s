0a150e75152da92e300b155cad0577846d8cbb5a
MrKiiw <ferarri2605@gmail.com>
2025-05-23 18:54:53 -0300

    agregue funciones basicas: rectangulo; pixel_x_y

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -1,53 +1,52 @@
-	.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
-	.equ BITS_PER_PIXEL,  	32
-
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
-
-	.globl main
+.include "funciones_basicas.s"
 
 main:
-	// x0 contiene la direccion base del framebuffer
- 	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+	mov x20, x0	// dirección base del framebuffer en x20 (por las dudas)
 
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+	/*  
+		x0 contiene la dirección base del framebuffer
+		x1 coordenada X inicio del dibujo
+		x2 coordenada Y inicio del dibujo
+		x5 cuántas líneas pintar en X
+  	    x6 cuántas líneas pintar en Y
+  	    x7 color
+  	*/
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
+// EJEMPLO:
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+	mov x1, 0
+	mov x2, 0
+	mov x5, SCREEN_WIDTH
+	mov x6, SCREEN_HEIGHT
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
+	movz x7, 0x0C, lsl 16
+	movk x7, 0x6F15, lsl 00
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
+	bl rectangulo
 
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
+	mov x1, 20
+	mov x2, 20
+	mov x5, 100
+	mov x6, 100
 
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
+	movz x7, 0xFFFF, lsl 16
+	movk x7, 0xFFFF, lsl 00
 
-	//---------------------------------------------------------------
-	// Infinite Loop
+	bl rectangulo
+
+	mov x1, 200
+	mov x2, 150
+	mov x5, 300
+	mov x6, 300
+
+	movz x7, 0x0C, lsl 16
+	movk x7, 0xFBAF, lsl 00
+
+	bl rectangulo
+
+// FIN DE EJEMPLO
 
 InfLoop:
+
 	b InfLoop
