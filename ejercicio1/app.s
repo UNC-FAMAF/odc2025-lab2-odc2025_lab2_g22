c80a36688d407e8c7f60d5f360fdf11231434790
MrKiiw <ferarri2605@gmail.com>
2025-06-02 23:29:54 -0300

    dibujo a jake y la hojita

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -16,40 +16,9 @@ main:
   	    x6 cuántas líneas pintar en Y
   	    x7 color
   	*/
-	/* EJEMPLO:
 
-	mov x1, 0
-	mov x2, 0
-	mov x5, SCREEN_WIDTH
-	mov x6, SCREEN_HEIGHT
-
-	movz x7, 0x0C, lsl 16
-	movk x7, 0x6F15, lsl 00
-
-	bl rectangulo
-
-	mov x1, 20
-	mov x2, 20
-	mov x5, 100
-	mov x6, 100
-
-	movz x7, 0xFFFF, lsl 16
-	movk x7, 0xFFFF, lsl 00
-
-	bl rectangulo
-
-	mov x1, 200
-	mov x2, 150
-	mov x5, 300
-	mov x6, 300
-
-	movz x7, 0x0C, lsl 16
-	movk x7, 0xFBAF, lsl 00
-
-	bl rectangulo
-
-// FIN DE EJEMPLO*/
 	bl fondo
+
 	mov x1, 200
 	mov x2, 150
 	mov x5, 300
@@ -59,6 +28,13 @@ main:
 	movk x7, 0xFBAF, lsl 00
 
 	//bl rectangulo
+
+	bl pintar_jake
+	
+    mov x1, 505
+    mov x2, 166
+    bl pintar_hojita
+
 b InfLoop
 
 InfLoop:
